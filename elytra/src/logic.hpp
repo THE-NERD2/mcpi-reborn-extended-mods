5e7f7ccb3fd6e5f480b3e0de535e93b77ed3ad2f
payton <pdp9729@gmail.com>
2024-06-11 12:02:13 -0600

    Fixed flying prototype movement function

--- elytra/src/logic.hpp
+++ elytra/src/logic.hpp
@@ -3,6 +3,15 @@
 static bool flying = false;
 static unsigned char* player = NULL;
 static unsigned char* level = NULL;
+static float t = clock();
+static float px = 0.0f;
+static float py = 0.0f;
+static float pz = 0.0f;
+static float dx = 0.0f;
+static float dy = 0.0f;
+static float dz = 0.0f;
+
+static float speed = 1.0f;
 
 HOOK(SDL_PollEvent, int, (SDL_Event* event)) {
     ensure_SDL_PollEvent();
@@ -10,7 +19,7 @@ HOOK(SDL_PollEvent, int, (SDL_Event* event)) {
     if(
         ret == 1 &&
         event != NULL &&
-        event->type == SDL_KEYDOWN &&
+        event->type == SDL_KEYUP &&
         event->key.keysym.sym == SDLK_SPACE &&
         player != NULL
     ) {
@@ -22,6 +31,12 @@ HOOK(SDL_PollEvent, int, (SDL_Event* event)) {
                 int z = round(*(float*) (player + Entity_z_property_offset));
                 if((*Level_getTile)(level, x, y - 3, z) == 0) {
                     flying = !flying;
+                    if(flying) {
+                        t = clock();
+                        px = *(float*) (player + Entity_x_property_offset);
+                        py = *(float*) (player + Entity_y_property_offset);
+                        pz = *(float*) (player + Entity_z_property_offset);
+                    }
                     fprintf(stderr, "Toggled flying\n");
                 }
             }
@@ -33,4 +48,30 @@ HOOK(SDL_PollEvent, int, (SDL_Event* event)) {
 static void update(unsigned char* minecraft) {
     player = *(unsigned char**) (minecraft + Minecraft_player_property_offset);
     level = *(unsigned char**) (minecraft + Minecraft_level_property_offset);
+    if(flying && player != NULL) {
+        float pitch = *(float*) (player + Entity_pitch_property_offset);
+        float yaw = *(float*) (player + Entity_yaw_property_offset);
+        float lookX = -sind(yaw);
+        float lookY = -sind(pitch);
+        float lookZ = cosd(yaw);
+        float l = sqrt(lookX * lookX + lookY * lookY + lookZ * lookZ);
+        lookX /= l;
+        lookY /= l;
+        lookZ /= l;
+        dx = lookX;
+        dy = lookY;
+        dz = lookZ;
+        int newT = clock();
+        float newX = px + dx * (float) (newT - t) / 1000000.0;
+        float newY = py + dy * (float) (newT - t) / 1000000.0;
+        float newZ = pz + dz * (float) (newT - t) / 1000000.0;
+        if((*Level_getTile)(level, round(newX), round(newY), round(newZ)) == 0 && (*Level_getTile)(level, round(newX), round(newY) - 1, round(newZ)) == 0) {
+            px = newX;
+            py = newY;
+            pz = newZ;
+            Entity_move_t player_move = *(Entity_move_t*) (*(unsigned char**) player + Entity_move_vtable_offset);
+            (*player_move)(player, dx, dy, dz);
+        }
+        t = newT;
+    }
 }
--- elytra/src/template.hpp
+++ elytra/src/template.hpp
@@ -17,6 +17,16 @@ static ArmorItem_t ArmorItem = (ArmorItem_t) 0x9362c;
 typedef ItemInstance* (*Player_getArmor_t)(unsigned char*, int);
 static Player_getArmor_t Player_getArmor = (Player_getArmor_t) 0x8fda4;
 
+typedef void (*Entity_move_t)(unsigned char*, float, float, float);
+static unsigned int Entity_move_vtable_offset = 0x18;
+
 unsigned char** Material_iron = (unsigned char**) 0x17a7a8;
+
+static float sind(float deg) {
+    return sinf(deg * M_PI / 180.0f);
+}
+static float cosd(float deg) {
+    return cosf(deg * M_PI / 180.0f);
+}
 
 #endif // #ifndef ELYTRA_MOD_TEMPLATE
