2286ee2efbd419988ea6d431c0e5946dc45bc9d1
payton <pdp9729@gmail.com>
2024-06-12 13:37:22 -0600

    Added rockets

--- elytra/src/items.hpp
+++ elytra/src/items.hpp
@@ -10,6 +10,16 @@ static void Item_initItems_injection(unsigned char* p0) {
     *(int*) (elytra + Item_category_property_offset) = 2;
     *(int*) (elytra + Item_max_damage_property_offset) = 250;
     *(int*) (elytra + Item_max_stack_size_property_offset) = 1;
+
+    unsigned char* rocket = (unsigned char*) ::operator new(0x34);
+    (*Item)(rocket, 158);
+    unsigned char* rvtable = *(unsigned char**) rocket;
+    Item_setIcon_t rocket_setIcon = *(Item_setIcon_t*) (rvtable + Item_setIcon_vtable_offset);
+    (*rocket_setIcon)(rocket, 6, 7);
+    *(int*) (elytra + Item_is_stacked_by_data_property_offset) = 1;
+    *(int*) (elytra + Item_category_property_offset) = 2;
+    *(int*) (elytra + Item_max_damage_property_offset) = 250;
+    *(int*) (elytra + Item_max_stack_size_property_offset) = 1;
 }
 static void Inventory_setupDefault_FillingContainer_addItem_call_injection(unsigned char* fc) {
     ItemInstance* elytra = new ItemInstance;
@@ -17,4 +27,10 @@ static void Inventory_setupDefault_FillingContainer_addItem_call_injection(unsig
 	elytra->auxiliary = 0;
 	elytra->id = 413;
 	(*FillingContainer_addItem)(fc, elytra);
+
+    ItemInstance* rocket = new ItemInstance;
+    rocket->count = 255;
+    rocket->auxiliary = 0;
+    rocket->id = 414;
+    (*FillingContainer_addItem)(fc, rocket);
 }
--- elytra/src/logic.hpp
+++ elytra/src/logic.hpp
@@ -9,6 +9,8 @@ static float dx = 0.0f;
 static float dy = 0.0f;
 static float dz = 0.0f;
 
+static bool rocketing = false;
+static int rEndTime = 0;
 static float speed = 0.0f;
 static const float old_velocity_decay = 0.8f;
 static const float drag = 0.0f;
@@ -16,7 +18,7 @@ static const float fall_speed = 0.5f;
 static const float pitch_accel = 4.0f;
 static const float terminal_velocity = 6.0f;
 //static const float turn_accel = WIP;
-//static const float rocket_boost = 2.0f;
+static const float rocket_boost = 4.0f;
 
 HOOK(SDL_PollEvent, int, (SDL_Event* event)) {
     ensure_SDL_PollEvent();
@@ -24,26 +26,34 @@ HOOK(SDL_PollEvent, int, (SDL_Event* event)) {
     if(
         ret == 1 &&
         event != NULL &&
-        event->type == SDL_KEYUP &&
-        event->key.keysym.sym == SDLK_SPACE &&
         player != NULL
     ) {
-        ItemInstance* armor = (*Player_getArmor)(player, 1);
-        if(armor != NULL) {
-            if(armor->id == 413) {
-                int x = round(*(float*) (player + Entity_x_property_offset));
-                int y = floor(*(float*) (player + Entity_y_property_offset));
-                int z = round(*(float*) (player + Entity_z_property_offset));
-                if((*Level_getTile)(level, x, y - 3, z) == 0 || flying) {
-                    flying = !flying;
-                    if(flying) {
-                        t = clock();
-                        speed = 0.0f;
-                        dx = 0.0f;
-                        dy = 0.0f;
-                        dz = 0.0f;
+        if(event->type == SDL_KEYUP && event->key.keysym.sym == SDLK_SPACE) {
+            ItemInstance* armor = (*Player_getArmor)(player, 1);
+            if(armor != NULL) {
+                if(armor->id == 413) {
+                    int x = round(*(float*) (player + Entity_x_property_offset));
+                    int y = floor(*(float*) (player + Entity_y_property_offset));
+                    int z = round(*(float*) (player + Entity_z_property_offset));
+                    if((*Level_getTile)(level, x, y - 3, z) == 0 || flying) {
+                        flying = !flying;
+                        if(flying) {
+                            t = clock();
+                            speed = 0.0f;
+                            dx = 0.0f;
+                            dy = 0.0f;
+                            dz = 0.0f;
+                        }
                     }
                 }
+            }
+        } else if(event->type == SDL_MOUSEBUTTONDOWN && event->button.button == SDL_BUTTON_RIGHT) {
+            unsigned char* inventory = *(unsigned char**) (player + Player_inventory_property_offset);
+            ItemInstance* selected = (*Inventory_getSelected)(inventory);
+            if(selected->id == 414) {
+                selected->count--;
+                rocketing = true;
+                rEndTime = clock() + 1000000;
             }
         }
     }
@@ -69,20 +79,26 @@ static void update(unsigned char* minecraft) {
 
         float ds = 0;
         ds -= drag * speed;
-        //fprintf(stderr, "%f\n", ds);
         ds += -(lookY + 0.1) * pitch_accel;
-        //fprintf(stderr, "%f\n", ds);
         speed += ds * dt;
-        //fprintf(stderr, "%f\n", speed);
         if(speed < 0) {
             speed = 0;
         } else if(speed > terminal_velocity) {
             speed = terminal_velocity;
         }
 
-        dx += lookX * speed * dt;
-        dy += lookY * speed * dt;
-        dz += lookZ * speed * dt;
+        if(rocketing) {
+            dx += lookX * (speed + rocket_boost) * dt;
+            dy += lookY * (speed + rocket_boost) * dt;
+            dz += lookZ * (speed + rocket_boost) * dt;
+            if(t >= rEndTime) {
+                rocketing = false;
+            }
+        } else {
+            dx += lookX * speed * dt;
+            dy += lookY * speed * dt;
+            dz += lookZ * speed * dt;
+        }
 
         Entity_move_t player_move = *(Entity_move_t*) (*(unsigned char**) player + Entity_move_vtable_offset);
         *(float*) (player + Player_jump_property_offset) = 0.0f;
